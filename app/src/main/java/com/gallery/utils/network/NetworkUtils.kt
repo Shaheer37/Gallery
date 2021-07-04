2db5a8c925ad016807e0c83ba51d2fb73ffb7cf0
shaheer37 <shaheermuzammil@gmail.com>
2021-07-04 18:59:16 +0500

    configured night mode colors

--- app/src/main/java/com/gallery/utils/network/NetworkUtils.kt
+++ app/src/main/java/com/gallery/utils/network/NetworkUtils.kt
@@ -7,8 +7,8 @@ import java.net.UnknownHostException
 private const val genericNetworkError = "An error occurred getting data from server."
 
 fun getNetworkErrorMessage(e: Exception) = when (e) {
-    is IOException -> "An Error occurred while processing your request. Please check your network connection and try again."
     is NoConnectivityException -> "You have no internet connection."
+    is IOException -> "An Error occurred. Please check your network connection and try again."
     is SocketTimeoutException -> "Request timed out. Try again."
     is UnknownHostException -> "Unable to connect to server."
     else -> genericNetworkError
