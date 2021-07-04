4cb971c68363be263f3c1f76a942fb2bd6db4a27
shaheer37 <shaheermuzammil@gmail.com>
2021-07-04 19:03:26 +0500

    image loading progress color set to colorOnPrimary in Detail screen

--- app/src/main/java/com/gallery/ui/binding/ImageBindings.kt
+++ app/src/main/java/com/gallery/ui/binding/ImageBindings.kt
@@ -1,9 +1,11 @@
 package com.gallery.ui.binding
 
 import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.gallery.R
 import com.gallery.data.model.PhotoData
 import com.gallery.ui.preview.PreviewViewModel
 
@@ -11,6 +13,7 @@ import com.gallery.ui.preview.PreviewViewModel
 fun previewImage(imageView: ImageView, photoData: PhotoData?, viewModel: PreviewViewModel) {
     photoData?.let { photoData ->
         val circularProgressDrawable = CircularProgressDrawable(imageView.context)
+        circularProgressDrawable.setColorSchemeColors(ResourcesCompat.getColor(imageView.context.resources, R.color.colorOnPrimary, null))
         circularProgressDrawable.strokeWidth = 5f
         circularProgressDrawable.centerRadius = 30f
         circularProgressDrawable.start()
